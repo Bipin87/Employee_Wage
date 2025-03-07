d6cc98d5f7d368983c8267ee94c325246d8d6e76
Bipin87 <sahubipin484@gmail.com>
2025-03-07 11:43:15 +0530

    [Bipin Kumar Sahu] Add: Ability to extend Employee Payroll Data added✨

--- employee_wage/EmployeePayroll.js
+++ employee_wage/EmployeePayroll.js
@@ -1,21 +1,40 @@
 class EmployeePayroll {
-    //Property
+    // Properties
     id;
-    name;
+    _name; // Private name property
     salary;
+    gender;
+    startDate;
+
     // Constructor
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
-        this.name = name;
+        this._name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+    // Getter and Setter for name
+    get name() { return this._name; }
+    set name(name) { this._name = name; }
+
+    // Method to return object details as a string
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate ? this.startDate.toLocaleDateString("en-US", options) : "undefined";
+        return `id=${this.id}, name='${this._name}', salary=${this.salary}, gender=${this.gender}, startDate=${empDate}`;
+    }
 }
 
-let emp1 = new EmployeePayroll(101, "Bipin Kumar Sahu", 150234);
-let emp2 = new EmployeePayroll(102, "Aman", 9000000);
-let emp3 = new EmployeePayroll(103, "Aryan", 540000);
-console.log(emp1.getDetails());
-console.log(emp2.getDetails());
-console.log(emp3.getDetails());
+// Creating EmployeePayroll object
+let employeePayrollData = new EmployeePayroll(1, "Bipin Kumar Sahu", 30000);
+console.log(employeePayrollData.toString());
+
+// Modifying employee name
+employeePayrollData.name = "Bipin";
+console.log(employeePayrollData.toString());
+
+// Creating a new EmployeePayroll object with gender and startDate
+let newEmployeePayrollData = new EmployeePayroll(2, "Aryan", 32000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
